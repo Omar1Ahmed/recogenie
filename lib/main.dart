e6ab54154e4fff835c2c07ca39e8bbf221380bac
Omar Ahmed <omarahmeddev89@gmail.com>
2025-07-27 03:38:48 +0300

    FB options, test Logging

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,17 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+import 'firebase_options.dart' show DefaultFirebaseOptions;
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(url: 'https://kkllntsdctvnypiezmfz.supabase.co', anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtrbGxudHNkY3R2bnlwaWV6bWZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1NTYwMTQsImV4cCI6MjA2OTEzMjAxNH0.omWRI8KqG72LELRJkCu-QOr8C7QqpeN2qcUbiYk-3HI');
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    name: 'Recogenie',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
 
   runApp(const MyApp());
