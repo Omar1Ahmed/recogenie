105f03803d691e2510fbc97d7a862ddf2e732bd3
Omar Ahmed <omarahmeddev89@gmail.com>
2025-07-27 04:04:03 +0300

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,19 +1,11 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:recogenie/firebase_options.dart';
 import 'package:recogenie/main.dart';
 
 void main() {
   // Bind the test framework to the Flutter engine
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  // Initialize Firebase before any tests
-  setUpAll(() async {
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-  });
 
   // Your test case
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
